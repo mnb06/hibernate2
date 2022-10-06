d19e92cc7014769998a29ec9eddf786d3caa65ea
m-cea <109054199+m-cea@users.noreply.github.com>
2022-10-06 12:24:21 -0300

    Implementada secuencia de la BD
    Importacion de librerias para implementar el ID serializable

--- Model/src/main/java/maven/model/Eti.java
+++ Model/src/main/java/maven/model/Eti.java
@@ -1,17 +1,25 @@
 package maven.model;
 
-
-import jakarta.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 
 @Entity  //Indica que es una entidad de la db
-@Table(name = "etiqueta") // Indica a que tabla esta haciendo referencia
+@Table(name = "eti") // Indica a que tabla esta haciendo referencia
 
-public class Eti {
+public class Eti{
 	
 	@Id
 	@Column(name = "ideti")
-	private int id;
-
+    @SequenceGenerator(name="entA", sequenceName="eti_ideti_seq", allocationSize = 1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="entA")
+    private Long ideti;
+	
+	
 	@Column(name = "nombre")
     private String nombre;
 
@@ -39,3 +47,4 @@ public class Eti {
 		this.descripcion = descripcion;
 	}
 }
+
