779463a0b78ec866da1f184f3260652043a4dea9
m-cea <109054199+m-cea@users.noreply.github.com>
2022-10-06 12:27:59 -0300

    Mas documentos insertados

--- Logic/src/main/java/view/docView.java
+++ Logic/src/main/java/view/docView.java
@@ -11,7 +11,10 @@ public class docView {
 
     private static void createDoc() {
         docController uc = new docController();
-        uc.createDoc("Documento 1", "Es el documento 1", "28/9/2022", "yo"); 
+        uc.createDoc("Documento 1", "Es el documento 1", "26/9/2022", "yo"); 
+        uc.createDoc("Documento 2", "Es el documento 2", "27/9/2022", "yo"); 
+        uc.createDoc("Documento 3", "Es el documento 3", "28/9/2022", "yo"); 
+        uc.createDoc("Documento 4", "Es el documento 4", "29/9/2022", "yo"); 
     }
 
 }
