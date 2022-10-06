ce05f55d6c5b6c8f320425d9d3c2da06aac73ee2
m-cea <109054199+m-cea@users.noreply.github.com>
2022-10-06 12:26:55 -0300

    Mas etiquetas insertadas

--- Logic/src/main/java/view/etiView.java
+++ Logic/src/main/java/view/etiView.java
@@ -12,6 +12,9 @@ public class etiView {
     private static void createEti() {
         etiController uc = new etiController();
         uc.createEti("Etiqueta 1", "Es la etiqueta 1"); 
+        uc.createEti("Etiqueta 2", "Es la etiqueta 2");
+        uc.createEti("Etiqueta 3", "Es la etiqueta 3");
+        uc.createEti("Etiqueta 4", "Es la etiqueta 4");
     }
 
 }
